da987d0fbeac571166fbdfcb83f75877fe0e1aa8
SKYGOD07 <sahilsharma361004@gmail.com>
2026-07-10 20:50:41 +0530

    feat(intelligence): implement Explainable AI Investigation Reasoning Engine

--- rakshastra_core/intelligence/__init__.py
+++ rakshastra_core/intelligence/__init__.py
@@ -9,6 +9,8 @@ from rakshastra_core.intelligence.audit_compliance import AuditComplianceEngine
 from rakshastra_core.intelligence.threat_intelligence import ThreatIntelligenceEngine, IntelligencePack
 from rakshastra_core.intelligence.graph_engine import GraphEngine
 from rakshastra_core.intelligence.timeline_engine import InvestigationTimelineEngine
+from rakshastra_core.intelligence.correlation_engine import MultiSourceCorrelationEngine
+from rakshastra_core.intelligence.explainable_reasoning import ExplainableReasoningEngine, LLMExplanationProvider
 
 __all__ = [
     "IntelligenceCollector",
@@ -22,5 +24,8 @@ __all__ = [
     "ThreatIntelligenceEngine",
     "IntelligencePack",
     "GraphEngine",
-    "InvestigationTimelineEngine"
+    "InvestigationTimelineEngine",
+    "MultiSourceCorrelationEngine",
+    "ExplainableReasoningEngine",
+    "LLMExplanationProvider"
 ]
